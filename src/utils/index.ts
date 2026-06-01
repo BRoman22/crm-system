fefe48ad32d655b841bc5c9b1c80c5c9ff677773
BRoman <rbondarev22@gmail.com>
2026-06-01 19:26:22 +0300

    переименовал functions в validateString

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export type { MetaResponse, Todo, TodoInfo, TodoInfoFilters } from './types';
 export { FILTER_LABELS, ENDPOINTS } from './constans';
-export { validateString } from './functions';
+export { validateString } from './validateString';
