766ec3db6e037a5447a11b20a3f7b5b27d42a6a2
BRoman <rbondarev22@gmail.com>
2026-06-02 12:33:01 +0300

    отступ между логическими блоками

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 export default function AddTodo({ fetchTasks }: Props) {
   const [title, setTitle] = useState<string>('');
+
   async function handleCreateTask(data: Pick<Todo, 'title' | 'isDone'>) {
     const validationError = validateString(data.title);
 
