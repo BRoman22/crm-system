29349a8ea6f6857acbc98c42da95f3b6b7fe4177
BRoman <rbondarev22@gmail.com>
2026-06-02 13:06:01 +0300

    добавил фигурные скобки в блоках if

--- src/utils/validateTitle.ts
+++ src/utils/validateTitle.ts
@@ -1,12 +1,17 @@
 export function validateTitle(string: string): string {
   const trimmedTitle = string.trim();
 
-  if (!trimmedTitle) return 'Поле обязательно для заполнения';
+  if (!trimmedTitle) {
+    return 'Поле обязательно для заполнения';
+  }
 
-  if (trimmedTitle.length < 2) return `Минимальная длина 2 символа (сейчас ${trimmedTitle.length})`;
+  if (trimmedTitle.length < 2) {
+    return `Минимальная длина 2 символа (сейчас ${trimmedTitle.length})`;
+  }
 
-  if (trimmedTitle.length > 64)
+  if (trimmedTitle.length > 64) {
     return `Максимальная длина 64 символа (сейчас ${trimmedTitle.length})`;
+  }
 
   return '';
 }
