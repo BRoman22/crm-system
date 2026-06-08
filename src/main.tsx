31ec073310b0e6db6b878daf2db11bbc3711030f
BRoman <rbondarev22@gmail.com>
2026-06-08 11:20:24 +0300

    заменил modern-normalize на normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize/modern-normalize.css';
+import 'normalize.css';
 import './index.css';
 import App from './App.tsx';
 
